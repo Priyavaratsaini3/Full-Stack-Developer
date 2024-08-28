d08aad542c322ea0c6e3fc20c45fbb81f2071049
Priyavaratsaini3 <143237627+Priyavaratsaini3@users.noreply.github.com>
2024-08-28 00:42:31 +0000

    completed

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,93 @@
+//let score = "33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let score = "33abc"
+// conversion string to number 
+//let score = true
+//let score = null
+let score = undefined
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33 string to number 
+// "33abc" => string to NaN (not a number)
+// "true" => 1 or "false" => 0
+// Null => 0
+// undefined => NaN
+
+let isLoggedIn = "priyavart"
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIN);
+
+// 1 => true ; 0 => false
+// "" => false
+// "priyavart" => true
+let someNumber = 3
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//                                                      Operations
+
+
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " priyavart"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 2) *2 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// Postfix 
+let num1 = 22
+let num2 = 34
+let num3 = num1 * num2
+//const num4 = num3++;
+//console.log(num4, num3);
+
+// Prefix
+const num4 = ++num3;
+console.log(num4, num3);
+
+
+
+
+
+
+
+
+
+
---
+++ .devcontainer/01_basics/05_datatypesummary.js
@@ -0,0 +1,21 @@
+// Primitive 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 200
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects , Function
